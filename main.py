22603db19066dc1a1f90f874627dc5caa5a29905
xaviermonica <xaviermonicaa123@gmail.com>
2024-01-13 04:44:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 
-st.sidebar.image("iris4.jpg", caption="Iris ", use_column_width=True)
+st.sidebar.image("iris4.jpg", use_column_width=True)
 
 # Sidebar Navigation with emojis 🌸 using Selectbox
 st.sidebar.title("🌼 Navigation")
