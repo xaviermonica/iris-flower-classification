27576ef0a01f01980ed26117b5aa8462d3bda35b
xaviermonica <xaviermonicaa123@gmail.com>
2024-01-24 23:16:00 +0530

    Update about.py

--- about.py
+++ about.py
@@ -21,7 +21,7 @@ def about_page():
             font-weight: bold;
         }
         .content {
-            background-color: #061a38;
+            background-color: #041329;
             border-radius: 10px;
             padding: 20px;
             box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
