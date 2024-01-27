edebf6ffeb774ed95b137108e8a390786890b19a
xaviermonica <xaviermonicaa123@gmail.com>
2024-01-27 13:17:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ if options == "🏠 Home":
     st.markdown(
         """
         <div style="
-        background-color: #f9f9f9; 
+        background-color: #bf8e8e; 
         padding: 40px;
         border-radius: 15px;
         box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
